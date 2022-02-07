be668b16c97141ce1d13606737ad0c57a5dda187
Bashir Abdulhakeem <freakybii@gmail.com>
2022-02-07 06:36:09 +0100

    pages-update-router-added-not-used

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
